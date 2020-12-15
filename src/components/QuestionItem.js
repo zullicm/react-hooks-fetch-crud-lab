3d3ad1a15a88375ac7d54eba56eccd9f8c861eb5
ihollander <ianholla@gmail.com>
2020-12-15 17:47:58 -0500

    Updated solution

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question, onDeleteClick }) {
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -13,13 +13,19 @@ function QuestionItem({ question, onDeleteClick }) {
     onDeleteClick(id);
   }
 
+  function handleAnswerChange(event) {
+    onAnswerChange(id, parseInt(event.target.value));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerChange}>
+          {options}
+        </select>
       </label>
       <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
